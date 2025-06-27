77a8eb450edaf6f2c6bd5415c0796662c22bd4f2
pallavi-2303 <singh.pallavikumari14@gmail.com>
2025-06-27 21:57:47 +0530

    Implement the menu section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 import About from './components/About';
 import Art from './components/Art';
+import Menu from './components/Menu';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
@@ -16,7 +17,7 @@ const App = () => {
  <Cocktails/>
  <About/>
  <Art/>
- <div className='w-full h-[300px] '></div>
+ <Menu/>
    </main>
   )
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { openingHours } from "../../constants";
+
+const Contact = () => {
+  return (
+    <footer id="contact">
+      <img
+        src="/fonts/images/footer-right-leaf.png"
+        alt="leaf-right"
+        id="f-right-leaf"
+      />
+      <img
+        src="/fonts/images/footer-left-leaf.png"
+        alt="leaf-left"
+        id="f-left-leaf"
+      />
+      <div className="content">
+        <h2>Where to Find Us</h2>
+        <div>
+          <h3>Visit Our Bar</h3>
+          <p>456, Raq Blvd. #404, Los Angeles, CA 90210</p>
+        </div>
+        <div>
+          <h3>Open Every Day</h3>
+          {openingHours.map((time) => (
+            <p key={time.day}>
+              {" "}
+              {time.day} : {time.time}
+            </p>
+          ))}
+        </div>
+        <div>
+          <h3>Socials</h3>
+          <div className="flex-center gap-5">
+            {socials.map((social) => (
+              <a
+                key={social.name}
+                href={social.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label={social.name}
+              >
+                <img src={social.icon} />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Contact;
